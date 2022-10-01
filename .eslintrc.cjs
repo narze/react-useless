5bc4e601c2029bbdaf5dc12a709f756ed07b60ef
Manassarn Manoonchai <manassarn@gmail.com>
2022-10-01 13:23:48 +0700

    Setup eslint for prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
