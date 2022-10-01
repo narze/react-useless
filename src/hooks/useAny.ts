7cdccf354e6986508af9a67f6cfce1b6b48f3ab5
Manassarn Manoonchai <manassarn@gmail.com>
2022-10-01 15:51:26 +0700

    Use DefinitelyNotAny

--- src/hooks/useAny.ts
+++ src/hooks/useAny.ts
@@ -1,7 +1,7 @@
 import { useMemo } from "react"
 
-export function useAny(value: any): any {
-  const result = useMemo(() => value as any, [])
+export function useAny(value: DefinitelyNotAny): DefinitelyNotAny {
+  const result = useMemo(() => value as DefinitelyNotAny, [])
 
   return result
 }
