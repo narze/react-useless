a4742f1e7859f5e72e0ce062b3338cb4d0ee233e
santhitak <santhita.sk@gmail.com>
2022-10-02 03:35:53 +0700

    style: fix typos in reactLogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import reactlogo from "./assets/react.svg"
+import reactLogo from "./assets/react.svg"
 import "./App.css"
 import { StoredHooks } from "./constants/StoredHooks"
 
@@ -10,7 +10,7 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactlogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>React Useless Hooks</h1>
