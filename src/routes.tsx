bb26ae06465c50091bcfc1c87d94c7f9f04f9ffd
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-10-02 00:25:51 +0700

    feat: add UseVoidDocs

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { NavLink, Route } from "react-router-dom"
 import UseCuteAndFunnyDocs from "./hooks/useCuteAndFunny/useCuteAndFunny.docs"
 import UseEvenDocs from "./hooks/useEven/useEven.docs"
 import UseLessDocs from "./hooks/useLess/useLess.docs"
+import UseVoidDocs from "./hooks/useVoid/useVoid.docs"
 
 const hookRoutes: { [key: string]: JSX.Element } = {
   useCuteAndFunny: <UseCuteAndFunnyDocs />,
   useLess: <UseLessDocs />,
   useEven: <UseEvenDocs />,
+  useVoid: <UseVoidDocs />,
 }
 
 export const renderHookRoutes = Object.keys(hookRoutes).map((hookName) => (
