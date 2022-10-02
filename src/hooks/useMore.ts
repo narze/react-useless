34f87f9c7c3132432a1b8f30e6ed99ab60662b81
First Kanisorn Sutham <kanisorn.sutham@unity3d.com>
2022-10-02 09:07:15 +0300

    chore: remove generated files

--- src/hooks/useMore.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"
-import { DefinitelyNotAny } from "./types"
-
-export function useMore(initialValue: DefinitelyNotAny) {
-  const [value, _setValue] = useState(initialValue)
-
-  return value
-}
