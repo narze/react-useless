14058bc4767637b2481ba42b18d3df8753c97640
Manassarn Manoonchai <manassarn@gmail.com>
2022-10-02 05:24:29 +0000

    Update template

--- src/hooks-usage/_TEMPLATE.tsx
+++ src/hooks-usage/_TEMPLATE.tsx
@@ -1,21 +1,33 @@
 // This file is a template, save this file as <yourHookName>.tsx and remove this comment
 
 import Card from "../components/Card"
+
 import { useLess } from "../hooks/useLess"
+import { useSalim } from "../hooks/useSalim"
 
 // Default export a functional component which includes hook's usage
 // You can use our Card component or roll your own container component
 export default function () {
   const cardProps = {
-    desc: "useLess - a useless hook that returns initial value.",
+    desc: "useYourHookName - returns something yada yada yada",
     examples: [
+      // For simple form, just pass the hook's return value
       { code: "const value = useLess(0)", value: useLess(0) },
-      {
-        code: 'const anotherValue = useLess("ඞ")',
-        value: useLess("ඞ"),
+
+      // Or, use function component form for more complex example with buttons
+      () => {
+        const { quote: salimQuote, refetch: salimRefetch } = useSalim()
+
+        return (
+          <>
+            <code>const {"{ quote, refetch }"} = useSalim()</code>
+            <div>quote is {salimQuote}</div>
+            <button onClick={() => salimRefetch()}>Click to refetch</button>
+          </>
+        )
       },
     ],
-    githubUsername: "narze",
+    githubUsername: "yourGithubUsername",
   }
 
   return <Card {...cardProps} />
