cefc8ea4076adbf69f13c575929768dcd2602c18
badgooooor <yuttakhanbaingen@gmail.com>
2022-10-02 15:44:42 +0700

    :sparkles: Add example how to spin right round

---
+++ src/hooks-usage/useYouSpinMyXRightRound.tsx
@@ -0,0 +1,33 @@
+import Card from "../components/Card"
+import { useYouSpinMyXRightRound } from "../hooks/useYouSpinMyXRightRound"
+
+export const UseYouSpinMyXRightRound = () => {
+  const [ref, spinMyXRightRound] = useYouSpinMyXRightRound(10)
+
+  return (
+    <button
+      ref={ref}
+      onClick={() => {
+        console.log("clicked")
+        spinMyXRightRound()
+      }}
+    >
+      Spin 🥩🌀
+    </button>
+  )
+}
+
+export default function () {
+  const cardProps = {
+    desc: "useYouSpinMyXRightRound - spin your element round 'n round",
+    examples: [
+      {
+        code: `const [ref, spinMyXRightRound] = useYouSpinMyXRightRound(10)`,
+        value: <UseYouSpinMyXRightRound />,
+      },
+    ],
+    githubUsername: "badgooooor",
+  }
+
+  return <Card {...cardProps} />
+}
