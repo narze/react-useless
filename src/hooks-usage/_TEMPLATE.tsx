91822aa4fbcda0acd7b5ca9ceee6362b2711eb5b
Manassarn "Noom" Manoonchai <248741+narze@users.noreply.github.com>
2022-10-02 12:30:03 +0700

    Extract components and use Vite meta glob import (#58)
    * Extract components and use Vite meta glob import
    
    * Hook husky install script
    
    * Add template component
    
    * Update readme
    
    * Update comment directing to hooks-usage folder
    
    * Update template

---
+++ src/hooks-usage/_TEMPLATE.tsx
@@ -0,0 +1,34 @@
+// This file is a template, save this file as <yourHookName>.tsx and remove this comment
+
+import Card from "../components/Card"
+
+import { useLess } from "../hooks/useLess"
+import { useSalim } from "../hooks/useSalim"
+
+// Default export a functional component which includes hook's usage
+// You can use our Card component or roll your own container component
+export default function () {
+  const cardProps = {
+    desc: "useYourHookName - returns something yada yada yada",
+    examples: [
+      // For simple form, just pass the hook's return value
+      { code: "const value = useLess(0)", value: useLess(0) },
+
+      // Or, use function component form for more complex example with buttons
+      () => {
+        const { quote: salimQuote, refetch: salimRefetch } = useSalim()
+
+        return (
+          <>
+            <code>const {"{ quote, refetch }"} = useSalim()</code>
+            <div>quote is {salimQuote}</div>
+            <button onClick={() => salimRefetch()}>Click to refetch</button>
+          </>
+        )
+      },
+    ],
+    githubUsername: "yourGithubUsername",
+  }
+
+  return <Card {...cardProps} />
+}
