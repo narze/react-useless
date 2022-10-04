77cdab909fe969290e2d5afbc04ba42b24a4e56e
coolkidneversleep <iiippbhy@gmail.com>
2022-10-04 11:09:43 +0700

    fix problem

--- src/hooks/useCapital.ts
+++ src/hooks/useCapital.ts
@@ -1,6 +1,9 @@
-export function useCapital(s: string) {
+export function useCapital(s: string | undefined = undefined) {
   if (s == undefined) {
-    document.querySelector("body").style.textTransform = "uppercase"
+    const body = document.querySelector("body")
+    if (body != null) {
+      body.style.textTransform = "uppercase"
+    }
   } else {
     return s.toUpperCase()
   }
