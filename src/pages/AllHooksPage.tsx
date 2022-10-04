13125be66cbe14758b846fa551e1ffa0a4bcd163
raksit31667 <raksit.m@ku.th>
2022-10-04 12:38:16 +0700

    🔥 remove irrelevant Vite logo

--- src/pages/AllHooksPage.tsx
+++ src/pages/AllHooksPage.tsx
@@ -447,9 +447,6 @@ function App() {
   return (
     <div className="App">
       <div className="flex justify-center gap-4">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
