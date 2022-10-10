c27ad81cbd5bda06a1508f4781bce7421b285bb8
wiennat <wiennat@gmail.com>
2022-10-10 22:20:53 +0900

    rename hook to useJQuery

--- src/hooks-usage/useJquery.tsx
+++ src/hooks-usage/useJquery.tsx
@@ -1,16 +1,16 @@
 import Card from "../components/Card"
-import { useJquery } from "../hooks/useJquery"
+import { useJQuery } from "../hooks/useJQuery"
 
 // Default export a functional component which includes hook's usage
 // You can use our Card component or roll your own container component
 export default function () {
   const cardProps = {
-    desc: "useJquery - but returning jquery object is too useful. It returns element selected by selector",
+    desc: "useJQuery - but returning a jQuery object is too useful. It returns element selected by selector instead",
     examples: [
       // 1.) For simple form, just pass the hook's return value
       {
-        code: 'const value = useJquery(".logo").map((m, idx) => <div key={idx}>{m.outerHTML}</div>)}',
-        value: useJquery(".logo").map((m, idx) => (
+        code: 'const value = useJQuery(".logo").map((m, idx) => <div key={idx}>{m.outerHTML}</div>)}',
+        value: useJQuery(".logo").map((m, idx) => (
           <div key={idx}>{m.outerHTML}</div>
         )),
       },
--- src/hooks/useJquery.ts
+++ src/hooks/useJquery.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export function useJquery(selector: string, deps: any = []) {
+export function useJQuery(selector: string, deps: any = []) {
   const [value, setValue] = useState<Array<Element>>([])
   useEffect(() => {
     setValue(() => [...document.querySelectorAll(selector)])
