045e3b3d2b4f63ec288c239fa7c48a85ec367502
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2022-10-23 11:38:41 +0700

    fix useSadSong hook usage component

--- src/hooks-usage/useSadSong.tsx
+++ src/hooks-usage/useSadSong.tsx
@@ -1,20 +1,25 @@
 import Card from "../components/Card"
 import { useSadSong } from "../hooks/useSadSong"
 
+const UseSadSong = () => {
+  const sadSong = useSadSong()
+
+  return (
+    <>
+      <button onClick={sadSong}>Click to play sad song</button>
+    </>
+  )
+}
+
 // Default export a functional component which includes hook's usage
 // You can use our Card component or roll your own container component
 export default function () {
   const cardProps = {
     desc: "useSadSong - Play your favorite sad song",
     examples: [
-      () => {
-        const sadSong = useSadSong()
-
-        return (
-          <>
-            <button onClick={sadSong}>Play Sad Song</button>
-          </>
-        )
+      {
+        code: "const sadSong = useSadSong()",
+        value: <UseSadSong />,
       },
     ],
     githubUsername: "phatsanphonna",
